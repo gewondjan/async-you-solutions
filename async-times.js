c01e1af5185a98aabba41d7a19928590d0535dde
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-10-12 15:00:56 -0600

    working on async times problem

---
+++ async-times.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+
+http = require('http');
+asynclib = require('async');
+
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+
+// asynclib.series([
+//     function(cb){
+//         asynclib.times(5, function(i, cb) {
+//             var opts
+//         });
+//     },
+//     function(cb) {
+
+//     }
+
+
+// ], 
+// function(error){
+//     if (error) {
+//         console.error(error);
+//         return;
+//     }
+// });
