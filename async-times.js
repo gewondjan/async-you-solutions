ce04a90d5b22abe2c49901d762f7e4a234ea4432
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-10-12 18:32:27 -0600

    updating asyn-times.js

--- async-times.js
+++ async-times.js
@@ -3,25 +3,35 @@
 http = require('http');
 asynclib = require('async');
 
-console.log(process.argv[2]);
-console.log(process.argv[3]);
+// console.log(process.argv[2]);
+// console.log(process.argv[3]);
 
 
-// asynclib.series([
-//     function(cb){
-//         asynclib.times(5, function(i, cb) {
-//             var opts
-//         });
-//     },
-//     function(cb) {
+var hostUrl = "http://" + process.argv[2] + ":" + process.argv[3];
 
-//     }
+asynclib.series([
+        function (cb) {
+            asynclib.times(5, function (i, cb) {
+                var opts = {
+                    hostname: hostUrl,
+                    path: '/users/create',
+                    method: 'POST'
+                };
+                http.request(opts, function (res) {
+                    res.on('data', function (portion) {
 
+                    });
+                });
 
-// ], 
-// function(error){
-//     if (error) {
-//         console.error(error);
-//         return;
-//     }
-// });
+            });
+        },
+        function (cb) {
+
+        }
+    ],
+    function (error) {
+        if (error) {
+            console.error(error);
+            return;
+        }
+    });
